2ac86590ccd78e578cea1de2014cc6d4d06c5f6a
zfar- <zfar.mah@gmail.com>
2022-10-13 19:43:27 +0200

    add remove_element code

---
+++ remove_element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        while val in nums:
+            nums.remove(val)
+            #print(nums)
+        
+        return(len(nums))
