6c4d655fc01e46e29c6c59442e8af423754727d2
zfar- <zfar.mah@gmail.com>
2024-09-21 15:49:18 +0200

    Range Sum Query - Immutable

--- easy/oneplus.py
+++ easy/oneplus.py
@@ -1,6 +1,10 @@
+import pandas as pd
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         s = [str(i) for i in digits]
         return [int(x) for x in str(int("".join(s)) + 1)]
         #res = [int(x) for x in str(num)]
         #print(res)
+
+    pd.read_csv()
